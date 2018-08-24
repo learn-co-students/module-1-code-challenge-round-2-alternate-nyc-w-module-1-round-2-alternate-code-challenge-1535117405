19fe11a0e0d21663a7d4de2e2f6a455ab005a49c
David <davidlago104@gmail.com>
2018-08-24 10:32:25 -0400

    Done

--- app/models/movie_screen.rb
+++ app/models/movie_screen.rb
@@ -1,8 +1,37 @@
 class MovieScreen
+  attr_reader :movie, :capacity
+
+  @@all = []
 
   def initialize(movie, capacity)
     @movie = movie
     @capacity = capacity
+    @@all << self
   end
 
-end
+  def self.all
+    @@all
+  end
+
+  def cars
+    title = DriveIn.all.select do |drive_in_object|
+      drive_in_object.moviescreen == self
+    end
+      title.map do |movie|
+        movie.car
+      end
+  end
+
+  # def add_car(name, car)
+  #   DriveIn.new(name, car, self)
+  # end
+
+  def how_many_viewers
+     total = cars.map do |car|
+       car.passenger_count
+    end
+    total.reduce(:+)
+  end
+
+
+end# end of class
